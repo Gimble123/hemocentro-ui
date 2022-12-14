9ecc4ac180aabe17e936682cd90f3a937cdf7f00
edstertz <edstertz@uolinc.com>
2022-12-14 11:47:27 -0300

    Criando tela home admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,21 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
+import { HomeAdminComponent } from './home-admin/home-admin.component';
+import { TabViewModule } from 'primeng/tabview';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeAdminComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ButtonModule,
-    InputTextModule
+    InputTextModule,
+    TabViewModule
   ],
   providers: [],
   bootstrap: [AppComponent]
