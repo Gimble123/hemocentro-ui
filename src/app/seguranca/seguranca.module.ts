c3120e62f7b0277c942a34b034f353152e0a7e64
edstertz <edstertz@uolinc.com>
2022-12-17 15:44:33 -0300

    Fazendo ajustes no css

--- src/app/seguranca/seguranca.module.ts
+++ src/app/seguranca/seguranca.module.ts
@@ -1,10 +1,10 @@
+import { FormLoginComponent } from './form-login/form-login.component';
 import { CoreModule } from './../core/core.module';
 import { InputTextModule } from 'primeng/inputtext';
 
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { LoginComponent } from './form-login/login.component';
 import { HomeAdminComponent } from './home-admin/home-admin.component';
 import { ResetSenhaComponent } from './reset-senha/reset-senha.component';
 
@@ -12,7 +12,7 @@ import { ResetSenhaComponent } from './reset-senha/reset-senha.component';
 
 @NgModule({
   declarations: [
-    LoginComponent,
+    FormLoginComponent,
     HomeAdminComponent,
     ResetSenhaComponent
   ],
@@ -21,6 +21,11 @@ import { ResetSenhaComponent } from './reset-senha/reset-senha.component';
     FormsModule,
     InputTextModule,
     CoreModule
+  ],
+  exports: [
+    FormLoginComponent,
+    HomeAdminComponent,
+    ResetSenhaComponent
   ]
 })
 export class SegurancaModule { }
