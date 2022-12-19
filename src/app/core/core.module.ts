a23e28ba66554a918514528b5585a6d16f3d707a
edstertz <edstertz@uolinc.com>
2022-12-19 09:55:41 -0300

    Criando navbar usuário

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,16 +1,18 @@
 import { NavbarComponent } from './navbar/navbar.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarUsuarioComponent } from './navbar-usuario/navbar-usuario.component';
 
 
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, NavbarUsuarioComponent],
   imports: [
     CommonModule
   ],
   exports: [
-    NavbarComponent
+    NavbarComponent,
+    NavbarUsuarioComponent
   ]
 })
 export class CoreModule { }
